196535f12f5890bdc85add529dac3abe8d1372fa
JeanJAKK <apotrekokoujean06@gmail.com>
2026-04-18 11:50:37 +0000

    fix: Error 50à

--- addons/marketplace/controllers/__init__.py
+++ addons/marketplace/controllers/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 from . import main
 from . import controllers
+from . import auth
+from . import cart
+from . import order
+from . import product
+from . import vendor
+from . import live
